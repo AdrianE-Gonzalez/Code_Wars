7b6d5c09388b3e7ca73621544b4219a5109a0e33
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2023-08-05 09:50:51 -0500

    Initial Commit

---
+++ Explosive_Sum_Aug_05/Explosive_Sum/Explosive_Sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using ull = unsigned long long;
+
+using namespace std;
+
+ull exp_sum(unsigned int n) {
+  return 0;
+}
+
+int main(){
+    
+    cout<< exp_sum(0)<<endl;
+    
+    return 0;
+}
