95c6a7ffcaeb58e04f32c2185f5e2d48cf0d8741
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2023-08-07 12:08:07 -0500

    Updated Code With Test Case 0-10

--- Explosive_Sum_Aug_05/Explosive_Sum/Explosive_Sum.cpp
+++ Explosive_Sum_Aug_05/Explosive_Sum/Explosive_Sum.cpp
@@ -5,12 +5,22 @@ using ull = unsigned long long;
 using namespace std;
 
 ull exp_sum(unsigned int n) {
-  return 0;
+  int temp=0;
+  //Base Case
+  if(n<1){
+    return 1;
+  }
+  //Recursion
+  else{
+    temp=exp_sum(n-1)+(n);
+  }
+  return temp;
 }
 
 int main(){
-    
-    cout<< exp_sum(0)<<endl;
+    for(int x=0; x<=10;x++){
+      cout<< x<<": "<<exp_sum(x)<<endl;
+    }
     
     return 0;
 }
