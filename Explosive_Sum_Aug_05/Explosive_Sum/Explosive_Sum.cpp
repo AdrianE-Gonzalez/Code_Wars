180856ee7a1c9639a4cea1c0e3c63b9fad0e8eb8
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2023-08-07 14:40:19 -0500

    Updated Variables

--- Explosive_Sum_Aug_05/Explosive_Sum/Explosive_Sum.cpp
+++ Explosive_Sum_Aug_05/Explosive_Sum/Explosive_Sum.cpp
@@ -5,22 +5,22 @@ using ull = unsigned long long;
 using namespace std;
 
 ull exp_sum(unsigned int n) {
-  int temp=0;
+  
   //Base Case
   if(n<1){
     return 1;
   }
   //Recursion
   else{
-    temp=exp_sum(n-1)+(n);
+    return exp_sum(n-1)+n;
+
   }
-  return temp;
+  
 }
 
 int main(){
     for(int x=0; x<=10;x++){
       cout<< x<<": "<<exp_sum(x)<<endl;
     }
-    
     return 0;
 }
