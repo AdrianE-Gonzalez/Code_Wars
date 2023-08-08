68ebc83f87ddb639dda1724de2da74d43eb017fb
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2023-08-08 12:28:34 -0500

    Initial Commit

---
+++ Snail_Aug_08/snail.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+std::vector<int> snail(const std::vector<std::vector<int>> &snail_map) {
+  return {};
+}
+
+int main(){
+    return 0;
+}
